de72fa43d5047be508593480c8c10b68710b963b
Ahmed AbdelMaksoud <A7medmaksoud@gmail.com>
2026-08-10 10:23:26 +0300

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "100mb",
+      bodySizeLimit: '100mb',
     },
+  },
+  serverExternalPackages: ['ffprobe-static', 'ffmpeg-static', 'fluent-ffmpeg'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/ffprobe-static/bin/**/*'],
   },
 };
 
